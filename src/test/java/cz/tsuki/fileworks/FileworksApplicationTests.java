8e1bb1a5a55122da0e48713f86af65477f5200ba
Karolína Šollová <karolina.sollova@arbes.com>
2024-01-23 14:05:15 +0100

    Initial commit

---
+++ src/test/java/cz/tsuki/fileworks/FileworksApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.tsuki.fileworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileworksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
